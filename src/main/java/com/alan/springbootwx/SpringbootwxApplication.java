f9b6126b515a793a815cbc8563bb6266b19eb0e3
alan <anshuaicn@163.com>
2019-04-03 14:16:50 +0800

    项目初始化

---
+++ src/main/java/com/alan/springbootwx/SpringbootwxApplication.java
@@ -0,0 +1,13 @@
+package com.alan.springbootwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwxApplication.class, args);
+    }
+
+}
